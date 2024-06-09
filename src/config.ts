73980487bf45437d34c300b3e2b65aebc0d13952
karthik <karthiktumala143@gmail.com>
2024-06-09 11:12:16 +0530

    rm string type frm config.js

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-const BASE_URL_:string = 'https://blog-post-api-dsam.onrender.com/api/v1';
-const LOCAL_URL:string = 'http://localhost:8000/api/v1'
-export {BASE_URL_,LOCAL_URL};
+const BASE_URL_ = 'https://blog-post-api-dsam.onrender.com/api/v1';
+const LOCAL_URL = 'http://localhost:8000/api/v1'
+export { BASE_URL_, LOCAL_URL };
