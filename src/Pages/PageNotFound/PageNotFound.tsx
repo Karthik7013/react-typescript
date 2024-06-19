58699401e287e2e52bbf3c93b58ddadbf28b128e
karthik <karthiktumala143@gmail.com>
2024-06-19 18:30:40 +0530

    added  svg fr login and logout pages

--- src/Pages/PageNotFound/PageNotFound.tsx
+++ src/Pages/PageNotFound/PageNotFound.tsx
@@ -1,12 +1,18 @@
-import { Stack } from '@mui/material'
-import React from 'react'
-import err from "../../assets/404 error with portals-amico.svg"
+import { Button, Stack, Toolbar, Typography } from '@mui/material'
 
 
+import err1 from "../../assets/pagenotfound.svg"
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
+import { Link } from 'react-router-dom';
 const PageNotFound = () => {
     return (
-        <Stack alignItems={'center'} justifyContent='center' sx={{ height: '100dvh', background: "#23a8fa" }}>
-            <img src={err} width="40%" height="40%" alt="" />
+        <Stack alignItems={'center'} justifyContent='center' sx={{ height: '100dvh' }}>
+            <img src={err1} width="60%" height="60%" alt="" />
+            <Typography variant='h5'>Sorry, Page not found</Typography>
+            <Stack direction="row" gap={2} alignItems="center">
+                <ArrowBackRoundedIcon fontSize='small' />
+                    <Typography textTransform="capitalize" variant='h6' >Home</Typography>
+            </Stack>
         </Stack>
     )
 }
