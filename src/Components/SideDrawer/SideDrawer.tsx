12d0e7fca1cd098dbc337821c3bac8f7fba9a394
karthik <karthiktumala143@gmail.com>
2024-06-20 18:25:59 +0530

    added svg's

--- src/Components/SideDrawer/SideDrawer.tsx
+++ src/Components/SideDrawer/SideDrawer.tsx
@@ -10,7 +10,8 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
-
+import { Avatar, Toolbar, Typography } from '@mui/material';
+import logo from "../../assets/logo.png";
 
 
 export default function TemporaryDrawer({ drawerOpen, setDrawerOpen }: any) {
@@ -21,6 +22,14 @@ export default function TemporaryDrawer({ drawerOpen, setDrawerOpen }: any) {
 
     const DrawerList = (
         <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+            <Toolbar>
+                <Avatar sizes='20' alt="Remy Sharp" src={logo}/> <Typography variant='h6' fontWeight={600} sx={{
+                        ml: 2, display: {
+                            xs: "none", md: 'block'
+                        }
+                    }}>Blogger Blog</Typography>
+            </Toolbar>
+            <Divider />
             <List>
                 {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
                     <ListItem key={text} disablePadding>
