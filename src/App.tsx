f554c63f1f958ef09874bae1da5043aba7eaa7e9
karthik <karthiktumala143@gmail.com>
2024-06-25 18:25:46 +0530

    added toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {  Box,Grid,Toolbar} from '@mui/material'
+import { BottomNavigation, BottomNavigationAction, Box, Grid, Paper, Toolbar } from '@mui/material'
 import { useEffect } from 'react'
 import axios from 'axios'
 import { useDispatch } from 'react-redux';
@@ -7,7 +7,9 @@ import { handleAlert, handleLoading, handlePosts } from './Redux/Actions/actions
 import SideBar from './Components/SideBar/SideBar';
 import RecentPosts from './Components/RecentPosts/RecentPosts';
 import ForYou from './Components/ForYou/ForYou';
-
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ArchiveIcon from '@mui/icons-material/Archive';
 const App = () => {
     const itemStyles = {
         display: {
@@ -29,7 +31,7 @@ const App = () => {
                 dispatch(handlePosts(res.data));
 
             } catch (error) {
-                dispatch(handleAlert({type:'error',message:'Internal Error',state:true}))
+                dispatch(handleAlert({ type: 'error', message: 'Internal Error', state: true }))
             } finally {
                 dispatch(handleLoading(false));
             }
@@ -47,6 +49,16 @@ const App = () => {
                 <Grid sx={{ height: 'calc(100dvh - 64px)', overflowY: 'scroll' }} item xs={18} lg={12} >
                     <RecentPosts />
                     <ForYou />
+                    <Toolbar />
+                    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+                        <BottomNavigation sx={{ display: { lg: 'none' } }}
+                            showLabels
+                        >
+                            <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+                            <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+                            <BottomNavigationAction label="Archive" icon={<ArchiveIcon />} />
+                        </BottomNavigation>
+                    </Paper>
                 </Grid>
                 <Grid item md={3} sx={itemStyles}>
                 </Grid>
